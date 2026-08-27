44e1e78b98cc87f138b85294dde8413924d989ed
Gui53 <gui53tube@gmail.com>
2026-08-27 13:48:59 -0300

    feat: criação de enum Categoria

---
+++ src/main/java/enums/CategoriaEnum.java
@@ -0,0 +1,16 @@
+package enums;
+
+/**
+ *
+ * @author guilh
+ */
+public enum CategoriaEnum {
+    ALIMENTAÇÃO,
+    ROUPA,
+    TRANSPORTE,
+    LAZER,
+    EDUCAÇÃO,
+    SAÚDE,
+    ASSINATURA,
+    OUTROS
+}
